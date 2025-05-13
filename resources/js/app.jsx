29ac9b77c91231a5b3493ee24badf047d41b286c
Nourhenezbidi <Nourhenezbidi210@gmail.com>
2025-05-13 13:32:11 +0100

    fetch

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+require('./bootstrap');
+ 
+require('alpinejs');
+ 
+import { createRoot } from 'react-dom/client';
+import App from "./Layouts/App";
+ 
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
